b9da0055816702f8ace827bffe436b2d2e39e3ae
PritamKundu <pritamkundu.pk5@gmail.com>
2022-11-16 03:32:41 +0600

    Elements Added

---
+++ cypress/e2e/Homepage_Test/TC_003 ContactUs_Test.js
@@ -0,0 +1,16 @@
+import { ContactUs } from "../../PageObjects/Homepage/TC_003 ContactUs";
+
+describe("Verify the 'Kontakt oss' menu at Navbar", ()=>{
+
+    const contactUs = new ContactUs();
+
+    before(()=>{
+        cy.openUrl()
+    })
+
+    it("Verify the 'Kontakt oss' menu at Navbar ", ()=>{
+        contactUs.contactUs('Kontakt oss')
+        })
+    })
+
+
