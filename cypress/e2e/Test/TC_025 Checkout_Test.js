4b0c70e04a385da0a8e1f7459f9ea0d34ed30e79
PritamKundu <pritamkundu.pk5@gmail.com>
2022-11-29 13:21:24 +0600

    Elements Added !

--- cypress/e2e/Test/TC_025 Checkout_Test.js
+++ cypress/e2e/Test/TC_025 Checkout_Test.js
@@ -8,7 +8,6 @@ describe("Verify product checkout page ", () => {
     cy.viewport(1280, 720)
   });
 
-  
   it("Verify product checkout page", () => {
     checkout.verifyCheckout(2, 'Baby Body Lyse grønn', 'M')
   });
