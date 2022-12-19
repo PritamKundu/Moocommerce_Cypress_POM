46418ec4f82bc6fd9918092d2e4a5c8e02d92e8d
PritamKundu <pritamkundu.pk5@gmail.com>
2022-12-19 20:58:12 +0600

    Test Elements Added !

--- cypress/e2e/Test/TC_022 Product_Category_Filter_Test.js
+++ cypress/e2e/Test/TC_022 Product_Category_Filter_Test.js
@@ -10,7 +10,6 @@ describe("Check the product filtering category ", () => {
     cy.visit('/shop')
     cy.viewport(1280, 720)
   });
-
   
   it("Verify the 'Ukategorisert' product category filtering", () => {
     menu.verifyMenus("Nettbutikk", "shop/");
